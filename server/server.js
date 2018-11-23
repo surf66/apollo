b0b1d5eddde1ccfa9de18c8b3ae7b46108f94a2d
Tom Pennington <tompennington.92@gmail.com>
2018-11-23 13:36:35 +0000

    Add missing dependency.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
